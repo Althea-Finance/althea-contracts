d984b1ea6cd09bac8e3c40d35e2c48e3f14ded3b
Tis <mr2z_4xc8@hotmail.com>
2024-03-25 21:41:49 +0100

    Deploy TheaToken first

---
+++ script/sepolia/02altheaToken.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import "lib/forge-std/src/Script.sol";
+import {TheaToken} from "src/dao/TheaToken.sol";
+import "script/sepolia/00constants.sol";
+
+contract AltheaCoreDeployment is Script {
+    TheaToken theaToken;
+
+    function setUp() public virtual {}
+
+    function run() public {
+
+        vm.startBroadcast();
+        deployTheaToken();
+        vm.stopBroadcast();
+
+    }
+
+    function deployTheaToken() internal {
+        uint8 sharedDecimals = 18;
+
+        theaToken = new TheaToken(
+            address(0), // Vault. Will be set later
+            address(LAYERZERO_ENDPOINT),
+            address(0), // TokenLocker. Will be set later
+            sharedDecimals
+        );
+    }
+
+
+}
