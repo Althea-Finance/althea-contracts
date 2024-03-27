7591b4805639f98e85db887681afde076300c868
jacobo <jacolansac@gmail.com>
2024-03-28 00:30:02 +0100

    audit comments on AllocationVesting.sol

--- src/token/AllocationVesting.sol
+++ src/token/AllocationVesting.sol
@@ -31,6 +31,7 @@ contract AllocationVesting is DelegatedOps {
     }
 
     struct AllocationState {
+        // @audit-info we could use some storage packing here to save gas. But not important
         address recipient;
         uint256 claimed; // in tokens
         uint256 allocationAtEndDate; // in tokens
@@ -48,6 +49,7 @@ contract AllocationVesting is DelegatedOps {
     constructor(ITheaToken _vestingToken, LinearVesting[] memory _allocations) {
         vestingToken = _vestingToken;
 
+        // @audit-issue it should be MaxSupply/2, not totalSupply. TotalSupply only increases with the mints when claiming
         totalAllocation = _vestingToken.totalSupply() / 2; // only vest 50% of the total supply, the rest are Emissions
         if (totalAllocation == 0) revert ZeroTotalAllocation();
 
@@ -61,6 +63,9 @@ contract AllocationVesting is DelegatedOps {
             uint256 startDate = _allocations[i].startDate;
             uint256 endDate = _allocations[i].endDate;
             if (allocationAtEndDate == 0) revert ZeroAllocationForWallet(recipient);
+            // @audit-issue past dates are not reverted here.
+            // @audit-info simplify this check to
+            // if (endDate < startDate) || (startDate < block.timestamp) revert ...
             if (startDate == 0 || endDate == 0 || startDate >= endDate || startDate == endDate)
                 revert IncompatibleVestingPeriod(startDate, endDate);
             if (allocations[recipient].allocationAtEndDate > 0) revert DuplicateAllocation();
@@ -75,6 +80,7 @@ contract AllocationVesting is DelegatedOps {
             );
 
             totalVesting += allocationAtEndDate;
+            // @audit instead of setting the variable to storage and reading it here, store it as a memory variable above, and only save it to storage once the function is finished
             if (totalVesting > totalAllocation) revert TooMuchAllocation();
 
             unchecked {
@@ -107,10 +113,13 @@ contract AllocationVesting is DelegatedOps {
             allocation.claimed
         );
         if (claimable == 0) revert NothingToClaim();
+
+        // @audit-info why not joining these two in the same line? claimedUpdated is not used again
         claimedUpdated = allocation.claimed + claimable;
         allocations[recipient].claimed = claimedUpdated;
 
         vestingToken.mintToAllocationVesting(recipient, claimable);
+        // @audit-info missing event here
 
         // We send to delegate for possible zaps
         //        vestingToken.transferFrom(vault, msg.sender, claimable);
@@ -133,6 +142,7 @@ contract AllocationVesting is DelegatedOps {
         );
     }
 
+    // @audit-info why not accepting the AllocationState object as input argument here? (and also in _vestedAt) as they are internal functions, they don't need the flexibility of multiple inputs . Or even accepting just the address
     function _claimableAt(
         uint256 when,
         uint256 allocationAtStartDate,
@@ -152,7 +162,10 @@ contract AllocationVesting is DelegatedOps {
         uint256 startDate,
         uint256 endDate
     ) private pure returns (uint256 vested) {
+        // @audit-info I guess we can remove many of these checks from here if we do thorough input validation in the constructor when creating the Structs
         if (when == 0 || startDate == 0 || endDate == 0 || startDate > endDate || allocationAtEndDate == 0) return 0;
+        
+        // this check is good though
         if (when < startDate) return 0;
 
         uint256 endTime = when >= endDate ? endDate : when;
