7ed409f857537fe503188c04f2532f11669c519e
Tis <mr2z_4xc8@hotmail.com>
2024-03-28 22:12:35 +0100

    simplified

--- src/token/AllocationVesting.sol
+++ src/token/AllocationVesting.sol
@@ -49,8 +49,7 @@ contract AllocationVesting is DelegatedOps {
     constructor(ITheaToken _vestingToken, LinearVesting[] memory _allocations) {
         vestingToken = _vestingToken;
 
-        // @audit-issue it should be MaxSupply/2, not totalSupply. TotalSupply only increases with the mints when claiming
-        totalAllocation = _vestingToken.totalSupply() / 2; // only vest 50% of the total supply, the rest are Emissions
+        totalAllocation = _vestingToken.maxTotalSupply() / 2; // only vest 50% of the total supply, the rest are Emissions
         if (totalAllocation == 0) revert ZeroTotalAllocation();
 
         uint256 loopEnd = _allocations.length;
@@ -63,10 +62,8 @@ contract AllocationVesting is DelegatedOps {
             uint256 startDate = _allocations[i].startDate;
             uint256 endDate = _allocations[i].endDate;
             if (allocationAtEndDate == 0) revert ZeroAllocationForWallet(recipient);
-            // @audit-issue past dates are not reverted here.
-            // @audit-info simplify this check to
-            // if (endDate < startDate) || (startDate < block.timestamp) revert ...
-            if (startDate == 0 || endDate == 0 || startDate >= endDate || startDate == endDate)
+
+            if (startDate >= endDate || startDate < block.timestamp)
                 revert IncompatibleVestingPeriod(startDate, endDate);
             if (allocations[recipient].allocationAtEndDate > 0) revert DuplicateAllocation();
 
@@ -97,32 +94,15 @@ contract AllocationVesting is DelegatedOps {
      */
     function claim(address account) external callerOrDelegated(account) {
         AllocationState memory allocation = allocations[account];
-        uint256 amount = _claim(allocation);
-        emit VestingClaimed(account, amount);
-    }
+        uint256 claimable = _vestedAt(block.timestamp, account) - allocation.claimed;
 
-    // This function exists to avoid reloading the AllocationState struct in memory
-    function _claim(AllocationState memory allocation) private returns (uint256 claimedUpdated) {
-        address recipient = allocation.recipient;
-        uint256 claimable = _claimableAt(
-            block.timestamp,
-            allocation.allocationAtStartDate,
-            allocation.allocationAtEndDate,
-            allocation.startDate,
-            allocation.endDate,
-            allocation.claimed
-        );
         if (claimable == 0) revert NothingToClaim();
 
-        // @audit-info why not joining these two in the same line? claimedUpdated is not used again
-        claimedUpdated = allocation.claimed + claimable;
-        allocations[recipient].claimed = claimedUpdated;
+        allocations[account].claimed = allocation.claimed + claimable;
 
-        vestingToken.mintToAllocationVesting(recipient, claimable);
-        // @audit-info missing event here
+        vestingToken.mintTo(account, claimable);
 
-        // We send to delegate for possible zaps
-        //        vestingToken.transferFrom(vault, msg.sender, claimable);
+        emit VestingClaimed(account, claimable);
     }
 
     /**
@@ -132,39 +112,22 @@ contract AllocationVesting is DelegatedOps {
      */
     function claimableNow(address account) external view returns (uint256 claimable) {
         AllocationState memory allocation = allocations[account];
-        claimable = _claimableAt(
-            block.timestamp,
-            allocation.allocationAtStartDate,
-            allocation.allocationAtEndDate,
-            allocation.startDate,
-            allocation.endDate,
-            allocation.claimed
-        );
+        claimable = _vestedAt(block.timestamp, account) - allocation.claimed;
     }
 
-    // @audit-info why not accepting the AllocationState object as input argument here? (and also in _vestedAt) as they are internal functions, they don't need the flexibility of multiple inputs . Or even accepting just the address
-    function _claimableAt(
-        uint256 when,
-        uint256 allocationAtStartDate,
-        uint256 allocationAtEndDate,
-        uint256 startDate,
-        uint256 endDate,
-        uint256 claimed
-    ) private pure returns (uint256) {
-        uint256 totalVested = _vestedAt(when, allocationAtStartDate, allocationAtEndDate, startDate, endDate);
-        return totalVested > claimed ? totalVested - claimed : 0;
+    function vestedAt(uint256 when, address account) external view returns (uint256) {
+        return _vestedAt(when, account);
     }
 
-    function _vestedAt(
-        uint256 when,
-        uint256 allocationAtStartDate,
-        uint256 allocationAtEndDate,
-        uint256 startDate,
-        uint256 endDate
-    ) private pure returns (uint256 vested) {
-        // @audit-info I guess we can remove many of these checks from here if we do thorough input validation in the constructor when creating the Structs
-        if (when == 0 || startDate == 0 || endDate == 0 || startDate > endDate || allocationAtEndDate == 0) return 0;
-        
+    function _vestedAt(uint256 when, address recipient) private view returns (uint256 vested) {
+        AllocationState memory allocation = allocations[recipient];
+        uint256 startDate = allocation.startDate;
+        uint256 endDate = allocation.endDate;
+        uint256 allocationAtStartDate = allocation.allocationAtStartDate;
+        uint256 allocationAtEndDate = allocation.allocationAtEndDate;
+
+        if (startDate >= endDate || allocationAtEndDate == 0) return 0;
+
         // this check is good though
         if (when < startDate) return 0;
 
