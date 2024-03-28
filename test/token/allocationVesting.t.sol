de32c38aaacb88ebbc290caf3f3f1414596458e0
Tis <mr2z_4xc8@hotmail.com>
2024-03-28 23:10:51 +0100

    More tests

--- test/token/allocationVesting.t.sol
+++ test/token/allocationVesting.t.sol
@@ -157,8 +157,7 @@ contract AllocationTest is BaseToken {
         uint256 allocationAtStartDate = coreContributor.allocationAtStartDate;
         uint256 allocationAtEndDate = coreContributor.allocationAtEndDate;
 
-        uint256 totalClaimed =
-            allocationAtStartDate +
+        uint256 totalClaimed = allocationAtStartDate +
             ((allocationAtEndDate - allocationAtStartDate) * (block.timestamp - startDate)) /
             (endDate - startDate);
 
@@ -173,5 +172,116 @@ contract AllocationTest is BaseToken {
         vm.warp(1797033600); // 12-06-2026 (2 years after start date)
         vestingContract.claim(coreContributor.recipient);
         assertEq(theaToken.balanceOf(coreContributor.recipient), allocationAtEndDate);
+    }
+
+    function testRandomCannotClaim() public {
+        address somebody = makeAddr("account");
+
+        vm.startPrank(somebody);
+
+        vm.warp(1733961600); //12-12-2024, after TGE
+        vm.expectRevert(AllocationVesting.NothingToClaim.selector);
+        vestingContract.claim(somebody);
+    }
+
+    function testCannotHaveDuplicateAllocations() public {
+        vm.startPrank(deployer);
+        AllocationVesting.LinearVesting[] memory allocations = new AllocationVesting.LinearVesting[](2);
+
+        allocations[0] = allAllocations[0];
+        allocations[1] = allAllocations[0];
+
+        vm.expectRevert(AllocationVesting.DuplicateAllocation.selector);
+        vestingContract = new AllocationVesting(ITheaToken(address(theaToken)), allocations);
+    }
+
+    function testCannotHaveMoreVestedThanHalfOfMaxSupply() public {
+        vm.startPrank(deployer);
+        AllocationVesting.LinearVesting[] memory newAllocations = new AllocationVesting.LinearVesting[](2);
+
+        uint256 halfTotalSupply = (100_000_000 / 2) * 10 ** 18;
+        newAllocations[0] = AllocationVesting.LinearVesting(
+            0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2,
+            halfTotalSupply,
+            0,
+            1718208000, //12-06-2024 (TGE)
+            1781222400 // 12-06-2026 (2 years after start date)
+        );
+
+        newAllocations[1] = AllocationVesting.LinearVesting(
+            0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266,
+            10 * 10 ** 18,
+            0,
+            1718208000, //12-06-2024
+            1749720258 // 12-06-2025
+        );
+
+        vm.expectRevert(AllocationVesting.TooMuchAllocation.selector);
+        vestingContract = new AllocationVesting(ITheaToken(address(theaToken)), newAllocations);
+    }
+
+    function testCannotHaveZeroAllocation() public {
+        vm.startPrank(deployer);
+        AllocationVesting.LinearVesting[] memory newAllocations = new AllocationVesting.LinearVesting[](2);
+
+        newAllocations[0] = AllocationVesting.LinearVesting(
+            0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2,
+            0,
+            0,
+            1718208000, //12-06-2024 (TGE)
+            1781222400 // 12-06-2026 (2 years after start date)
+        );
+
+        newAllocations[1] = AllocationVesting.LinearVesting(
+            0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266,
+            100 * 10 ** 18,
+            0,
+            1718208000, //12-06-2024
+            1749720258 // 12-06-2025
+        );
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                AllocationVesting.ZeroAllocationForWallet.selector,
+                0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+            )
+        );
+        vestingContract = new AllocationVesting(ITheaToken(address(theaToken)), newAllocations);
+    }
+
+    function testIncompatibleVestingPeriod_sameStartAndEndDate() public {
+        vm.startPrank(deployer);
+        AllocationVesting.LinearVesting[] memory newAllocations = new AllocationVesting.LinearVesting[](1);
+
+        newAllocations[0] = AllocationVesting.LinearVesting(
+            0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2,
+            10 * 10 ** 18,
+            0,
+            1718208000, //12-06-2024 (TGE)
+            1718208000 // 12-06-2024
+        );
+
+        vm.expectRevert(
+            abi.encodeWithSelector(AllocationVesting.IncompatibleVestingPeriod.selector, 1718208000, 1718208000)
+        );
+        vestingContract = new AllocationVesting(ITheaToken(address(theaToken)), newAllocations);
+    }
+
+    function testIncompatibleVestingPeriod_startAfterEndDate() public {
+        vm.startPrank(deployer);
+        AllocationVesting.LinearVesting[] memory newAllocations = new AllocationVesting.LinearVesting[](1);
+
+        newAllocations[0] = AllocationVesting.LinearVesting(
+            0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2,
+            10 * 10 ** 18,
+            0,
+            1718208000, //12-06-2024 (TGE)
+            1718064000 // 11-06-2024
+        );
+
+        vm.expectRevert(
+            abi.encodeWithSelector(AllocationVesting.IncompatibleVestingPeriod.selector, 1718208000, 1718064000)
+        );
+        vestingContract = new AllocationVesting(ITheaToken(address(theaToken)), newAllocations);
     }
 }
