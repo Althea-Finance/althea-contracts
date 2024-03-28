0dfdc0c83d4c34e561d5f6983db533e1679eb4df
Tis <mr2z_4xc8@hotmail.com>
2024-03-28 22:30:10 +0100

    more tests for allocation vesting

--- test/token/allocationVesting.t.sol
+++ test/token/allocationVesting.t.sol
@@ -11,8 +11,9 @@ contract AllocationTest is BaseToken {
     AllocationVesting.LinearVesting[] allAllocations;
 
     constructor() {
-        // Early supporter
         uint8 DECIMALS = 18;
+
+        // Early supporter, with tokens at TGE
         allAllocations.push(
             AllocationVesting.LinearVesting(
                 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266,
@@ -20,6 +21,28 @@ contract AllocationTest is BaseToken {
                 125000 * 10 ** DECIMALS,
                 1718208000, //12-06-2024
                 1749720258 // 12-06-2025
+            )
+        );
+
+        // Core contributor, 0% at TGE and 6 months cliff
+        allAllocations.push(
+            AllocationVesting.LinearVesting(
+                0x23618E81e3f5a4B2a83D6431a60359aa88b7c025,
+                5000000 * 10 ** DECIMALS,
+                0,
+                1733961600, //12-12-2024 (6 months after TGE)
+                1797033600 // 12-12-2026 (2 years after start date)
+            )
+        );
+
+        // Treasury, 0% at TGE linear vesting, no cliff
+        allAllocations.push(
+            AllocationVesting.LinearVesting(
+                0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2,
+                2500000 * 10 ** DECIMALS,
+                0,
+                1718208000, //12-06-2024 (TGE)
+                1781222400 // 12-06-2026 (2 years after start date)
             )
         );
     }
@@ -96,7 +119,7 @@ contract AllocationTest is BaseToken {
         assertEq(theaToken.balanceOf(earlySupporter.recipient), totalClaimed);
 
         // At end date, should be able to claim all
-        vm.warp(1749720258); //1-12-2024
+        vm.warp(1749720258); //12-6-2025
         vestingContract.claim(earlySupporter.recipient);
 
         totalClaimed =
@@ -106,6 +129,49 @@ contract AllocationTest is BaseToken {
 
         assertEq(theaToken.balanceOf(earlySupporter.recipient), totalClaimed);
         assertEq(totalClaimed, allocationAtEndDate);
-        
+
+        // next day, nothing to claim
+        vm.warp(1749772800); //13-06-2025
+        vm.expectRevert(AllocationVesting.NothingToClaim.selector);
+        vestingContract.claim(earlySupporter.recipient);
+    }
+
+    function testCoreContributor() public {
+        AllocationVesting.LinearVesting storage coreContributor = allAllocations[1];
+
+        vm.startPrank(coreContributor.recipient);
+
+        vm.warp(1733875200); //11-12-2024, nothing to claim yet
+        vm.expectRevert(AllocationVesting.NothingToClaim.selector);
+        vestingContract.claim(coreContributor.recipient);
+
+        vm.warp(1733961600); //12-12-2024
+        // 0% at TGE, should not be able to claim
+        vm.expectRevert(AllocationVesting.NothingToClaim.selector);
+        vestingContract.claim(coreContributor.recipient);
+
+        // 6 months later, should be able to claim
+        vm.warp(1749686400); //12-06-2025
+        uint256 startDate = coreContributor.startDate;
+        uint256 endDate = coreContributor.endDate;
+        uint256 allocationAtStartDate = coreContributor.allocationAtStartDate;
+        uint256 allocationAtEndDate = coreContributor.allocationAtEndDate;
+
+        uint256 totalClaimed =
+            allocationAtStartDate +
+            ((allocationAtEndDate - allocationAtStartDate) * (block.timestamp - startDate)) /
+            (endDate - startDate);
+
+        vestingContract.claim(coreContributor.recipient);
+        assertEq(theaToken.balanceOf(coreContributor.recipient), totalClaimed);
+
+        // cannot claim anymore in the same moment
+        vm.expectRevert(AllocationVesting.NothingToClaim.selector);
+        vestingContract.claim(coreContributor.recipient);
+
+        // last day, should be able to claim all
+        vm.warp(1797033600); // 12-06-2026 (2 years after start date)
+        vestingContract.claim(coreContributor.recipient);
+        assertEq(theaToken.balanceOf(coreContributor.recipient), allocationAtEndDate);
     }
 }
